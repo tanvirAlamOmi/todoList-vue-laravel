7638d4c4bc35a3084872d3503ff638850083f876
tanvirAlam <araromi1995>
2021-01-22 07:01:05 +0600

    spelling correction

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\ItemController;
+use App\Http\Controllers\ItemController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
